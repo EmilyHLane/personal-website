85f7a5c6a7323905af23e2e8f8bf9190e1a6f346
EMILYHLANE <embem80@gmail.com>
2018-12-17 21:56:06 -0800

    adds show/hide project description on mouse in/out and on focus in/out

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,28 @@
 // alert("javascript ready");
+
+//show project description on hover
+const image = document.querySelectorAll(".proj-img");
+const imageLink = document.querySelectorAll(".image-link");
+
+for (let i = 0; i < image.length; i++) {
+  image[i].addEventListener("mouseenter", addRemoveDescription);
+  image[i].addEventListener("mouseleave", addRemoveDescription);
+}
+
+for (let i = 0; i < imageLink.length; i++) {
+  imageLink[i].addEventListener("focusin", focusAddRemoveDescription);
+  imageLink[i].addEventListener("focusout", focusAddRemoveDescription);
+}
+
+function addRemoveDescription() {
+  console.log("mouse event");
+  const description = this.children[0];
+  description.classList.toggle("project-description");
+  description.classList.toggle("project-remove-desc");
+}
+
+function focusAddRemoveDescription() {
+  const focusDescription = this.children[0].children[0];
+  focusDescription.classList.toggle("project-description");
+  focusDescription.classList.toggle("project-remove-desc");
+}
